088c2d4c8f9fbea8c84009a56819a256b2d9aa0e
charanmutyala <charanmutyala4@gmail.com>
2023-07-28 16:02:19 +0530

    biography

---
+++ data.js
@@ -0,0 +1,212 @@
+ReactDOM.render(
+  <div>
+    <h1>
+      <i>Elon Reeve Musk</i>
+    </h1>
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg"
+      height="400px"
+      width="350px"
+    />
+    <h4>Born in June 28, 1971 (age 52) Pretoria, Transvaal, South Africa</h4>
+    <h4> Founder, CEO, and chief engineer of SpaceX</h4>
+
+    <p style={{ "background-color":"blue" }}>
+      Elon Reeve Musk is a business magnate and investor. He is the founder,
+      chairman, CEO, and chief technology officer of SpaceX; angel investor, CEO
+      and product architect of Tesla, Inc.; owner and CTO of Twitter; founder of
+      the Boring Company; co-founder of Neuralink and OpenAI; and president of
+      the Musk Foundation. Musk is the wealthiest person in the world with an
+      estimated net worth of US$239 billion as of July 2023, according to the
+      Bloomberg Billionaires Index and $248.8 billion according to Forbes's Real
+      Time Billionaires list, primarily from his ownership stakes in Tesla and
+      SpaceX.
+    </p>
+
+    <p>
+      Musk was born in Pretoria, South Africa, and briefly attended the
+      University of Pretoria before moving to Canada at age 18, acquiring
+      citizenship through his Canadian-born mother. Two years later, he
+      matriculated at Queen's University in Kingston, Ontario, and two years
+      after that transferred to the University of Pennsylvania, where he
+      received bachelor's degrees in economics and physics. He moved to
+      California in 1995 to attend Stanford University. After two days, he
+      dropped out and, with his brother Kimbal, co-founded the online city guide
+      software company Zip2. The startup was acquired by Compaq for $307 million
+      in 1999, and with $12 million of the money he made, that same year Musk
+      co-founded X.com, a direct bank. X.com merged with Confinity in 2000 to
+      form PayPal.
+    </p>
+    <h2 style={{ textDecoration:" underline" }}>
+       Early life
+    </h2>
+    {/* <h2 style={"text-decoration:underline;"}> Early life</h2> */}
+    <p>
+      Elon Reeve Musk was born on June 28, 1971, in Pretoria, one of South
+      Africa's capital cities.Musk has British and Pennsylvania Dutch ancestry.
+      His mother is Maye Musk (née Haldeman), a model and dietitian born in
+      Saskatchewan, Canada, and raised in South Africa. His father, Errol Musk,
+      is a South African electromechanical engineer, pilot, sailor, consultant,
+      and property developer, who partly owned a Zambian emerald mine near Lake
+      Tanganyika. Musk has a younger brother, Kimbal, and a younger sister,
+      Tosca.
+    </p>
+    <p>
+      Musk's family was wealthy during his youth.His father was elected to the
+      Pretoria City Council as a representative of the anti-apartheid
+      Progressive Party and has said that his children shared their father's
+      dislike of apartheid.His maternal grandfather, Joshua Haldeman, was an
+      American-born Canadian who took his family on record-breaking journeys to
+      Africa and Australia in a single-engine Bellanca airplane.After his
+      parents divorced in 1980, Musk chose to mostly live with his father.Musk
+      regretted his decision and has become estranged from his father. He has a
+      paternal half-sister and a half-brother.
+    </p>
+
+    <h2>
+      <u>Education</u>
+    </h2>
+
+    <p>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/PBHS-facade.jpg/330px-PBHS-facade.jpg" />
+      <br></br>
+      Musk attended Waterkloof House Preparatory School, Bryanston High School,
+      and Pretoria Boys High School, from where he graduated.Musk applied for a
+      Canadian passport through his Canadian-born mother, knowing that it would
+      be easier to immigrate to the United States this way.While waiting for his
+      application to be processed, he attended the University of Pretoria for
+      five months. Musk arrived in Canada in June 1989 and lived with a second
+      cousin in Saskatchewan for a year,working odd jobs at a farm and lumber
+      mill.In 1990, he entered Queen's University in Kingston, Ontario.Two years
+      later, he transferred to the University of Pennsylvania (UPenn), where he
+      completed studies for a Bachelor of Arts degree in physics and a Bachelor
+      of Science degree in economics from the Wharton School
+    </p>
+
+    <h2>
+      <u>Business career</u>
+    </h2>
+    <p>
+      n 1995, Musk, his brother Kimbal, and Greg Kouri founded Zip2.Errol Musk
+      provided them with $28,000 in funding.The company developed an Internet
+      city guide with maps, directions, and yellow pages, and marketed it to
+      newspapers.They worked at a small rented office in Palo Alto,Musk coding
+      the website every night.Eventually, Zip2 obtained contracts with The New
+      York Times and the Chicago Tribune.The brothers persuaded the board of
+      directors to abandon a merger with CitySearch;however, Musk's attempts to
+      become CEO were thwarted.Compaq acquired Zip2 for $307 million in cash in
+      February 1999,and Musk received $22 million for his 7-percent share.
+    </p>
+    <h2>
+      <u>SpaceX</u>
+    </h2>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Charles_Bolden_congratulates_SpaceX_CEO_and_Chief_Designer_Elon_Musk_in_front_of_the_historic_Dragon_capsule.jpg/255px-Charles_Bolden_congratulates_SpaceX_CEO_and_Chief_Designer_Elon_Musk_in_front_of_the_historic_Dragon_capsule.jpg" />
+    <p>
+      In early 2001, Musk became involved with the nonprofit Mars Society and
+      discussed funding plans to place a growth-chamber for plants on Mars. In
+      October of the same year, he traveled to Moscow with Jim Cantrell and Adeo
+      Ressi to buy refurbished intercontinental ballistic missiles (ICBMs) that
+      could send the greenhouse payloads into space. He met with the companies
+      NPO Lavochkin and Kosmotras; however, Musk was seen as a novice and the
+      group returned to the United States empty-handed. In February 2002, the
+      group returned to Russia with Mike Griffin to look for three ICBMs. They
+      had another meeting with Kosmotras and were offered one rocket for $8
+      million, which Musk rejected. He instead decided to start a company that
+      could build affordable rockets.With $100 million of his own money, Musk
+      founded SpaceX in May 2002 and became the company's CEO and Chief Engineer
+    </p>
+
+    <p>
+      SpaceX attempted its first launch of the Falcon 1 rocket in 2006.Though
+      the rocket failed to reach Earth orbit, it was awarded a Commercial
+      Orbital Transportation Services program contract from NASA Administrator
+      (and former SpaceX consultant) Mike Griffin later that year. After two
+      more failed attempts that nearly caused Musk and his companies to go
+      bankrupt,SpaceX succeeded in launching the Falcon 1 into orbit in
+      2008.Later that year, SpaceX received a $1.6 billion Commercial Resupply
+      Services contract from NASA for 12 flights of its Falcon 9 rocket and
+      Dragon spacecraft to the International Space Station, replacing the Space
+      Shuttle after its 2011 retirement.In 2012, the Dragon vehicle docked with
+      the ISS, a first for a commercial spacecraft
+    </p>
+    <h3>
+      <u>Starlink</u>
+    </h3>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Starlink_Mission_%2847926144123%29.jpg/330px-Starlink_Mission_%2847926144123%29.jpg" />
+    <p>
+      In 2015, SpaceX began development of the Starlink constellation of
+      low-Earth-orbit satellites to provide satellite Internet access, with the
+      first two prototype satellites launched in February 2018. A second set of
+      test satellites, and the first large deployment of a piece of the
+      constellation, occurred in May 2019, when the first 60 operational
+      satellites were launched.The total cost of the decade-long project to
+      design, build, and deploy the constellation is estimated by SpaceX to be
+      about $10 billion.Some critics, including the International Astronomical
+      Union, have alleged that Starlink blocks the view of the sky and poses a
+      collision threat to spacecraft
+    </p>
+    <p>
+      During the Russian invasion of Ukraine, Musk sent Starlink terminals to
+      Ukraine to provide Internet access and communication.However, Musk refused
+      to block Russian state media on Starlink, declaring himself "a free speech
+      absolutist". In October 2022, Musk stated that about 20,000 satellite
+      terminals had been donated to Ukraine, together with free data transfer
+      subscriptions, which cost SpaceX $80 million. After asking the United
+      States Department of Defense to pay for further units and future
+      subscriptions on behalf of Ukraine, Musk publicly stated that SpaceX would
+      continue to provide Starlink to Ukraine for free, at a yearly cost to
+      itself of $400 million
+    </p>
+    <h3>
+      <u>Tesla</u>
+    </h3>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Elon_Musk%2C_Tesla_Factory%2C_Fremont_%28CA%2C_USA%29_%288765031426%29.jpg/330px-Elon_Musk%2C_Tesla_Factory%2C_Fremont_%28CA%2C_USA%29_%288765031426%29.jpg" />
+    <p>
+      Tesla, Inc.—originally Tesla Motors—was incorporated in July 2003 by
+      Martin Eberhard and Marc Tarpenning, who financed the company until the
+      Series A round of funding. Both men played active roles in the company's
+      early development prior to Musk's involvement.Musk led the Series A round
+      of investment in February 2004; he invested $6.5 million, became the
+      majority shareholder, and joined Tesla's board of directors as
+      chairman.Musk took an active role within the company and oversaw Roadster
+      product design but was not deeply involved in day-to-day business
+      operations
+    </p>
+    <p>
+      Following a series of escalating conflicts in 2007, and the financial
+      crisis of 2007–2008, Eberhard was ousted from the firm.Musk assumed
+      leadership of the company as CEO and product architect in 2008.A 2009
+      lawsuit settlement with Eberhard designated Musk as a Tesla co-founder,
+      along with Tarpenning and two others. As of 2019, Musk was the
+      longest-tenured CEO of any automotive manufacturer globally. In 2021, Musk
+      nominally changed his title to "Technoking" while retaining his position
+      as CEO
+    </p>
+
+    <h3>
+      <u>SolarCity and Tesla Energy</u>
+    </h3>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Pair_of_2009_SolarCity_Dodge_Sprinters.JPG/330px-Pair_of_2009_SolarCity_Dodge_Sprinters.JPG" /><br></br>
+    <p style={{ display: "inline" }}>
+      Musk provided the initial concept and financial capital for SolarCity,
+      which his cousins Lyndon and Peter Rive founded in 2006.By 2013, SolarCity
+      was the second largest provider of solar power systems in the United
+      States. In 2014, Musk promoted the idea of SolarCity building an advanced
+      production facility in Buffalo, New York, triple the size of the largest
+      solar plant in the United States.Construction of the factory started in
+      2014 and was completed in 2017. It operated as a joint venture with
+      Panasonic until early 2020.<br></br>
+      <br></br>
+      Tesla acquired SolarCity for over $2 billion in 2016 and merged it with
+      its battery unit to create Tesla Energy. The deal's announcement resulted
+      in a more than 10% drop in Tesla's stock price. At the time, SolarCity was
+      facing liquidity issues.Multiple shareholder groups filed a lawsuit
+      against Musk and Tesla's directors, claiming that the purchase of
+      SolarCity was done solely to benefit Musk and came at the expense of Tesla
+      and its shareholders.Tesla directors settled the lawsuit in January 2020,
+      leaving Musk the sole remaining defendant.Two years later, the court ruled
+      in Musk's favor
+    </p>
+  </div>,
+  document.getElementById("contents")
+);
